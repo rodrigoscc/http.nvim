e03845e5b3e201c4d21500880ff8fb5a0e98a8fb
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2024-12-01 05:56:04 -0500

    fix: update grammar to support requests without newline and url encoded values with @

--- lua/http-nvim/queries.lua
+++ lua/http-nvim/queries.lua
@@ -3,7 +3,7 @@ local Job = require("plenary.job")
 local M = {}
 
 local grammar_repo_url = "https://github.com/rstcruzo/tree-sitter-http2"
-local grammar_repo_revision = "aa0494f48bee402a13e653a9173863bbf86d8902"
+local grammar_repo_revision = "279762bf7302c55a268c49e484771546445266b4"
 
 local function clone_grammar_repo(path, callback)
     local job = Job:new({
