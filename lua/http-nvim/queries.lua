acf714d21fb0d3163d3487c926cb71363b898128
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2024-12-28 19:37:19 -0500

    fix: update grammar to fix new lines in json body

--- lua/http-nvim/queries.lua
+++ lua/http-nvim/queries.lua
@@ -3,7 +3,7 @@ local Job = require("plenary.job")
 local M = {}
 
 local grammar_repo_url = "https://github.com/rstcruzo/tree-sitter-http2"
-local grammar_repo_revision = "97fcbdd4d1901bd5b0745f23044762b6372c6890"
+local grammar_repo_revision = "1c12a93bae8359c68345e94be7563c2b533159b1"
 
 local function clone_grammar_repo(path, callback)
     local job = Job:new({
